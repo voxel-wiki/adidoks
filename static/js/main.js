0d090cc1b937bcde44c5f29659dcbc843a0e61bb
PQCraft <58464017+PQCraft@users.noreply.github.com>
2022-07-13 04:57:50 -0400

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,22 @@
-// Set darkmode
-document.getElementById('mode').addEventListener('click', () => {
+// Set darkmode (and enable it by default for systems using darkmode)
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    
+    if (localStorage.getItem('theme') === null) {
+        document.body.classList.toggle('dark');
+        localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+    }
+    
+}
 
+document.getElementById('mode').addEventListener('click', () => {
+    
     document.body.classList.toggle('dark');
     localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
-  
+    
 });
   
 if (localStorage.getItem('theme') === 'dark') {
   
-  document.body.classList.add('dark');
+    document.body.classList.add('dark');
   
 }
